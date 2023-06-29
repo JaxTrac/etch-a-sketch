86749dd236a8323af934b7182d754541cdc2b7c6
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-06-29 18:49:48 +0200

    Create divs as columns

--- script.js
+++ script.js
@@ -3,8 +3,12 @@
 
 function createDivs(size) {
     for (let i = 0; i < size; i++) {
-        let newDiv = document.createElement("div");
-        document.body.appendChild(newDiv);
+        let rowDivs = document.createElement("div");
+        document.body.appendChild(rowDivs);
+        for (let j = 0; j < size; j++) {
+            let columnDivs = document.createElement("div");
+            rowDivs.appendChild(columnDivs);
+        }
     }
 }
 
