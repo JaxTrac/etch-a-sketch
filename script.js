355f3644856237932652518314540d0f3742a8a7
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-07-03 17:17:46 +0200

    Add classes to rows/columns

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 function createDivs(size) {
     for (let i = 0; i < size; i++) {
         let rowDivs = document.createElement("div");
+        rowDivs.classList.add("rows");
         document.body.appendChild(rowDivs);
         for (let j = 0; j < size; j++) {
             let columnDivs = document.createElement("div");
+            columnDivs.classList.add("columns")
             rowDivs.appendChild(columnDivs);
         }
     }
